ca1ca6681338170c31255b0571d0036be9596f53
Brendan Dileo <dileob23@gmail.com>
2025-07-18 10:53:15 -0400

    Setup trie class

--- src/main/java/org/example/Trie.java
+++ src/main/java/org/example/Trie.java
@@ -1,5 +1,32 @@
 package org.example;
 
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * 
+ */
 public class Trie {
+
+    // Represents a single node in the Trie
+    // Each node contains a map of its child nodes 
+    // and a boolean flag indicating if it marks the end of a word
+    private static class TrieNode {
+        Map<Character, TrieNode> children = new HashMap<>();
+        boolean isWord = false;
+    }
+
+    private final TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+
+
+
+
+
+
     
 }
