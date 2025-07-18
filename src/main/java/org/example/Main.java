f3a8f4db1b6f7b0673a9ec57403997a242a95887
Brendan Dileo <dileob23@gmail.com>
2025-07-18 14:30:16 -0400

    Update main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,37 @@
 package org.example;
 
 public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        Trie trie = new Trie();
+
+        // Insert
+        trie.insert("hello");
+        trie.insert("hell");
+        trie.insert("heaven");
+        trie.insert("heavy");
+        trie.insert("hero");
+        trie.insert("her");
+        trie.insert("batman");
+        trie.insert("bat");
+
+
+        System.out.println(trie);
+        
+
+
+
+
+
+    }
+
+
+
+
+
+
+
 }
